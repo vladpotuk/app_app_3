d2d0a632c910eb7f8f8d81e22b5ca879d744e141
vladpotuk <vladpotuk01@gmail.com>
2024-03-24 14:40:01 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
 ﻿
+    public class StudentGrades
+    {
+        public string Name { get; }
+        public int[] Grades { get; }
+
+        public StudentGrades(string name, int[] grades)
+        {
+            Name = name;
+            Grades = grades;
+        }
+
+        public double GetAverageGrade()
+        {
+            return Grades.Length > 0 ? Grades.Average() : 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StudentGrades[] students = new StudentGrades[]
+            {
+            new StudentGrades("Анна", new int[] {85, 90, 95}),
+            new StudentGrades("Денис", new int[] {75, 80, 85}),
+            new StudentGrades("Влад", new int[] {90, 95, 100}),
+            new StudentGrades("Настя", new int[] {70, 75, 80})
+            };
+
+            StudentGrades studentWithMaxAverageGrade = students.OrderByDescending(s => s.GetAverageGrade()).FirstOrDefault();
+
+            if (studentWithMaxAverageGrade != null)
+            {
+                Console.WriteLine($"Студент з максимальною середньою оцінкою: {studentWithMaxAverageGrade.Name}");
+                Console.WriteLine($"Середня оцінка: {studentWithMaxAverageGrade.GetAverageGrade()}");
+            }
+            else
+            {
+                Console.WriteLine("Не вдалося знайти студента.");
+            }
+        }
+    }
+
