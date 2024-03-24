1cc14429de5829159b2fd714825ca3e989b58b9d
vladpotuk <vladpotuk01@gmail.com>
2024-03-24 13:16:24 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+public static class StringExtensions
+{
+    public static int CountVowels(this string input)
+    {
+        int count = 0;
+        foreach (char c in input.ToLower())
+        {
+            if ("aeiouAEIOU".Contains(c))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text1 = "Hello World";
+        string text2 = "This is a test string";
+
+        Console.WriteLine($"Number of vowels in '{text1}': {text1.CountVowels()}"); 
+        Console.WriteLine($"Number of vowels in '{text2}': {text2.CountVowels()}"); 
+    }
+}
+
