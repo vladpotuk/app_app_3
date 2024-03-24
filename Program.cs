72249bfbf7a4cda1fdf580d41ddb50321b215acb
vladpotuk <vladpotuk01@gmail.com>
2024-03-24 14:30:13 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+
+public static class StringExtensions
+{
+    public static int LengthOfLastWord(this string input)
+    {
+        
+        string[] words = input.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+        
+        if (words.Length == 0)
+            return 0;
+
+       
+        return words[words.Length - 1].Length;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        string[] testStrings = { "This is a test string.", "Hello, world!", "   Trim   the   spaces.   ", "NoLastWord" };
+
+        foreach (string str in testStrings)
+        {
+            Console.WriteLine($"The length of the last word in \"{str}\" is: {str.LengthOfLastWord()}");
+        }
+    }
+}
+
