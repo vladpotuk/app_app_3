b82aa6bcaa077692f7eefb836c1f297294288148
vladpotuk <vladpotuk01@gmail.com>
2024-03-24 13:08:19 +0200

    update

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@
 public static class IntExtensions
 {
     public static Func<int, bool> IsEven = (number) => number % 2 == 0;
+
+    public static bool IsEvenNumber(this int number)
+    {
+        return IsEven(number);
+    }
 }
 
 class Program
@@ -12,8 +17,7 @@ class Program
         int num1 = 5;
         int num2 = 10;
 
-        Console.WriteLine($"{num1} is even: {IntExtensions.IsEven(num1)}"); 
-        Console.WriteLine($"{num2} is even: {IntExtensions.IsEven(num2)}"); 
+        Console.WriteLine($"{num1} is even: {num1.IsEvenNumber()}"); 
+        Console.WriteLine($"{num2} is even: {num2.IsEvenNumber()}"); 
     }
 }
-
