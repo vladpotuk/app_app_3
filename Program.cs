df44d4a0af4d135712d0b71f88b85834c076eec2
vladpotuk <vladpotuk01@gmail.com>
2024-03-24 12:58:12 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+public static class IntExtensions
+{
+    public static Func<int, bool> IsOdd = (number) => number % 2 != 0;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        int num2 = 10;
+
+        Console.WriteLine($"{num1} is odd: {IntExtensions.IsOdd(num1)}");
+        Console.WriteLine($"{num2} is odd: {IntExtensions.IsOdd(num2)}"); 
+    }
+}
+
