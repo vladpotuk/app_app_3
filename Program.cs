1f4fa80c6e43f25172a3cffc315235b40d51278e
vladpotuk <vladpotuk01@gmail.com>
2024-03-24 14:28:30 +0200

    upadte

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+public static class StringExtensions
+{
+    public static int WordCount(this string input)
+    {
+        
+        string[] words = input.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+  
+        return words.Length;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        string[] testStrings = { "This is a test string.", "Hello, world!", "   Trim   the   spaces.   " };
+
+        foreach (string str in testStrings)
+        {
+            Console.WriteLine($"The number of words in \"{str}\" is: {str.WordCount()}");
+        }
+    }
+}
