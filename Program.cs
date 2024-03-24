cc3ac75e88840464cfbd6023fcda270801d94b87
vladpotuk <vladpotuk01@gmail.com>
2024-03-24 13:13:41 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+
+public static class IntExtensions
+{
+    public static bool IsPrime(this int number)
+    {
+        if (number <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        int num2 = 10;
+
+        Console.WriteLine($"{num1} is prime: {num1.IsPrime()}"); 
+        Console.WriteLine($"{num2} is prime: {num2.IsPrime()}"); 
+    }
+}
+
