a55824691860bec36a318321c6289ac23ee94915
vladpotuk <vladpotuk01@gmail.com>
2024-03-24 13:01:57 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+public static class IntExtensions
+{
+    public static Func<int, bool> IsEven = (number) => number % 2 == 0;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        int num2 = 10;
+
+        Console.WriteLine($"{num1} is even: {IntExtensions.IsEven(num1)}"); 
+        Console.WriteLine($"{num2} is even: {IntExtensions.IsEven(num2)}"); 
+    }
+}
+
